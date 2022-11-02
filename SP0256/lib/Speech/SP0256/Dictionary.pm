a89e5c37bac864fa1360aff40152713f19a0d9ee
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2022-11-02 18:01:28 +0000

    Version 0.01
    First demo version

---
+++ SP0256/lib/Speech/SP0256/Dictionary.pm
@@ -0,0 +1,34 @@
+package Speech::SP0256::Dictionary;
+
+sub initDic{
+	my $self=shift;
+	$self->{dic}={
+		" "=>"PA3",
+		","=>"PA4",
+		"."=>"PA5",
+		0=>"ZZ YR OW",
+		1=>"WW SX AX NN1",
+		2=>"TT2 UW2",
+		3=>"TH RR1 IY",
+		4=>"FF FF OR",
+		5=>"FF FF AY VV",
+		6=>"SS SS IH IH PA3 KK2 SS",
+		7=>"SS SS EH EH VV IH NN1",
+		8=>"EY PA3 TT2",
+		9=>"NN1 AA AY NN1",
+		10=>"TT2 EH EH NN1",
+		11=>"IH LL EH EH VV IH NN1",
+		12=>"TT2 WH EH EH LL VV",
+		13=>"TH ER1 PA2 PA3 TT2 IY NN1",
+		14=>"FF OR PA2 PA3 TT2 IY NN1",
+		15=>"FF IH FF PA2 PA3 TT2 IY NN1",
+		16=>"SS SS IH PA2 PA3 TT2 IY NN1",
+		17=>"SS SS EH VV IH NN1 PA2 PA3 TT2 IY NN1",
+		18=>"EY PA2 PA3 TT2 IY NN1",
+		19=>"NN1 AY NN1 PA2 PA3 TT2 IY NN1",
+		20=>"TT2 WH EH EH NN1 PA2 PA3 TT2 IY",
+	};
+
+}
+
+1;
---
+++ SP0256/lib/Speech/SP0256/Rules.pm
@@ -0,0 +1,58 @@
+package Speech::SP0256::Rules;
+
+sub setRules{
+	my ($self)=@_;
+	$self->{rules}=[
+       ["ght"   , "TT1"  ],
+	   ["dge\$" , "JH"   ],
+	   ["^i\$"  , "AY"   ],
+	   ["^un"  , "AA NN" ],
+	   ["age"  ,  "EY JH"],
+
+	   
+	   # for those that don't fit, the remaining letters are
+	   # directly translated
+	   [ "a","EH"],
+		["b","BB1"],
+		["c","KK1"],
+		["d","DD1"],
+		["e","EH"],
+		["f","FF"],
+		["g","GG1"],
+		["h","HH1"],
+		["i","IH"],
+		["j","JH"],
+		["k"," KK1 "],
+		["l","LL"],
+		["m","MM"],
+		["n","NN1"],
+		["o","OW"],
+		["p","PP"],
+		["q","KK2"],
+		["r","RR1"],
+		["s","SS"],
+		["t","TT1"],
+		["u","UW1"],
+		["v","VV"],
+		["w","WW"],
+		["x","KK2 SS"],
+		["y","IH"],
+		["z","ZZ"],
+	 ]
+	   
+	   
+}
+
+sub addRule{
+	my ($self,$match,$result)=@_;
+	$self->{rules}=[sort ruleSort([$match,$result],@{$self->{rules}})];
+}
+
+sub ruleSort{
+	if (length $a->[0] > length $b->[0]) {return -1}
+	elsif (length $a->[0] == length $b->[0]){return 0}
+	else  {return 1}	
+}
+
+
+1;
